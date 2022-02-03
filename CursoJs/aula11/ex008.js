3075fdb3178b93831c545aa570dbe6ccc156b229
Nicolas0786 <98752585+Nicolas0786@users.noreply.github.com>
2022-02-03 12:32:29 -0300

    Add files via upload

---
+++ CursoJs/aula11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('O print na tela em node so funciona com console')
+
+var velocidade = 80.2
+
+console.log(`A velocidade do seu veiculo é ${velocidade} KM/h`);
+if(velocidade > 60){
+    console.log('Voe utrapassou o limite de velocidade, MULTADO!!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
